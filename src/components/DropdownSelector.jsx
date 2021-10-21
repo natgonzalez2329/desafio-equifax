9f7f8e43d1d1e947aee6b65ee25c48055c70fe95
francisca <fran.munozm93@gmail.com>
2021-10-21 17:36:05 -0300

    UPDATE DropdownSelector.css

--- src/components/DropdownSelector.jsx
+++ src/components/DropdownSelector.jsx
@@ -5,13 +5,13 @@ import "./DropdownSelector.css";
 
 const DropdownSelector = () => {
   const options = [
-    { name: "Swedish", value: "sv" },
-    { name: "English", value: "en" },
-    { name: "Chino", value: "ch" },
-    { name: "Portuges", value: "pr" },
-    {
-      items: [{ name: "Spanish", value: "es" }],
-    },
+    { name: "Plugin 1", value: "sv" },
+    { name: "Plugin 2", value: "en" },
+    { name: "Plugin 3", value: "ch" },
+    { name: "Plugin 4", value: "pr" },
+    // {
+    //   items: [{ name: "Spanish", value: "es" }],
+    // },
   ];
   return (
     <SelectSearch
