6a6d4b43ada3eba53089e088efa2c5ffe25a34fb
Natasha Gonzalez <82616634+natgonzalez2329@users.noreply.github.com>
2021-10-21 08:28:10 -0300

    layout, add component footer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import ConfigPluginModified from "./components/ConfigPluginModified";
 import DropdownSelector from "./components/DropdownSelector";
 import EditPluginList from "./components/EditPluginList";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <div className="row content-fluid">
+    <div className="App content-fluid">
+      <div className="row">
         <Header />
       </div>
-      <div className="row content-fluid">
+      <div className="row">
         <div className="col-3">
           <DropdownSelector />
         </div>
@@ -20,6 +21,9 @@ function App() {
         <div className="col-6">
           <ConfigPluginModified />
         </div>
+      </div>
+      <div className="row">
+        <Footer />
       </div>
     </div>
   );
